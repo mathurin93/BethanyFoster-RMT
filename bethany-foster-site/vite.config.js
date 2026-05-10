d1f866ff2ce49922a76fb8efdc3ff280b2149eaa
Matt Robinson <mathurin_robinson@ymail.com>
2026-05-09 23:03:04 -0400

    second push

--- bethany-foster-site/vite.config.js
+++ bethany-foster-site/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // This matches your GitHub repository name exactly
+  base: '/BethanyFoster-RMT/', 
   plugins: [
     react(),
     tailwindcss(),
